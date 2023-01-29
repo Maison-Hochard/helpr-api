dd7d45d2717cd002c73916fc61d97c5f6a5eb6ad
HugoRCD <hugo.richard@epitech.eu>
2023-01-29 11:33:07 +0100

    Look like it work

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -97,6 +97,8 @@ export class UserService {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 7,
       path: "/",
+      sameSite: "none",
+      secure: true,
     });
     return formatUser(user);
   }
