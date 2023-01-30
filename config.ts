b39242df333e04f69148ea977df14c61df720dc1
HugoRCD <hugo.richard@epitech.eu>
2023-01-30 12:40:40 +0100

    Add new env

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 export const config = () => ({
-  env: process.env.NODE_ENV,
+  env: process.env.APP_ENV,
   jwt: {
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    access_token_secret: process.env.AUTH_TOKEN_SECRET,
     refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
-    access_token_expiration: process.env.ACCESS_TOKEN_EXPIRATION,
+    access_token_expiration: process.env.AUTH_TOKEN_EXPIRATION,
     refresh_token_expiration: process.env.REFRESH_TOKEN_EXPIRATION,
   },
   google: {
