4a999432da6bd1536bde5fdd47e562ce38125e20
HugoRCD <hugo.richard@epitech.eu>
2023-02-25 17:11:46 +0100

    Update Helpr Provider

--- prisma/seed/providers/helpr/helprProvider.ts
+++ prisma/seed/providers/helpr/helprProvider.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../../../seed";
 import {
   every10MinutesTrigger,
-  every30MinutesTrigger,
+  everyDayTrigger,
   everyHourTrigger,
 } from "./helprTriggers";
 
@@ -10,12 +10,12 @@ export async function createHelprProvider() {
     data: {
       name: "Helpr",
       description:
-        "Helpr provide many default triggers like every 5 minutes, every 10 minutes, etc...",
-      logo: "https://storage.cloud.google.com/helpr/helpr-logo-white.svg",
+        "Helpr provide many default triggers like every 10 minutes, everyday, etc...",
+      logo: "helpr-logo",
     },
   });
   // Triggers
   await every10MinutesTrigger(provider.id);
-  await every30MinutesTrigger(provider.id);
   await everyHourTrigger(provider.id);
+  await everyDayTrigger(provider.id);
 }
