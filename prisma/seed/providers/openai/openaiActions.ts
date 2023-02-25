5e66a849edeb4542b618dc56f8e3d1800f2c25f4
HugoRCD <hugo.richard@epitech.eu>
2023-02-25 16:51:21 +0100

    Update OpenAI Provider

--- prisma/seed/providers/openai/openaiActions.ts
+++ prisma/seed/providers/openai/openaiActions.ts
@@ -3,27 +3,31 @@ import { prisma } from "../../../seed";
 export async function createCompletionAction(providerId: number) {
   return await prisma.action.create({
     data: {
-      title: "Create Completion",
-      description: "Create Completion with OpenAI",
+      title: "Generate content",
+      description: "Generate content with OpenAI's powerful completion engine.",
       endpoint: "openai",
       name: "create-completion",
+      premium: true,
       providerId: providerId,
       variables: {
         create: [
           {
-            title: "Create Completion Model",
-            name: "create_completion_model",
-            value: "{openai_completion_model}",
+            title: "Model",
+            key: "openai_model",
+            value: "{openai_model}",
+            type: "select",
           },
           {
-            title: "Create Completion Prompt",
-            name: "create_completion_prompt",
-            value: "{openai_completion_prompt}",
+            title: "Prompt",
+            key: "openai_prompt",
+            value: "{openai_prompt}",
+            type: "textarea",
           },
           {
-            title: "Create Completion Size",
-            name: "create_completion_size",
-            value: "{openai_completion_size}",
+            title: "Max tokens",
+            key: "openai_max_tokens",
+            value: "{openai_max_tokens}",
+            type: "number",
           },
         ],
       },
